4fc686ff74d1cd89589a45657e63d7687c8af730
mremannn <67206374+mremannn@users.noreply.github.com>
2025-09-05 16:18:31 -0600

    changed theme, and cleaned up files, also laid ground work for DAP but nothing has been used

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,9 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-	  vim.cmd.colorscheme("tokyonight-night")
-  end,
+	"nyoom-engineering/oxocarbon.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {},
+	config = function()
+		vim.cmd.colorscheme("oxocarbon")
+	end,
 }
