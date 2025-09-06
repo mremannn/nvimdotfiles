f02e445d6b5d13df4df9a8ca286ba9395ecbfb8c
mremannn <67206374+mremannn@users.noreply.github.com>
2025-09-06 17:32:12 -0600

    updated oil.nvim configuration

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,11 @@ return {
 	"stevearc/oil.nvim",
 	opts = {},
 	config = function()
-		require("oil").setup()
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+			},
+		})
 		vim.keymap.set("n", "<C-n>", vim.cmd.Oil)
 	end,
 	lazy = false,
