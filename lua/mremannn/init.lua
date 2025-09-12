eb0c3679c4b4c8fd2904d3e11f636cb63862d9e2
mremannn <67206374+mremannn@users.noreply.github.com>
2025-09-11 21:54:48 -0600

    reformat lol

--- lua/mremannn/init.lua
+++ lua/mremannn/init.lua
@@ -1,24 +1,34 @@
+require("mremannn.lazy")
 require("mremannn.lspdefine")
 require("mremannn.lualinetheme")
+-- set map leader (kinda redundant but whatever)
 vim.g.mapleader = " "
+
 -- number setup
 vim.o.number = true
 vim.o.relativenumber = true
+
 -- setup / search stuff
 vim.o.hlsearch = false
 vim.o.incsearch = true
+
 -- prevents from less than 10 lines being off the screen
 vim.o.scrolloff = 10
+
 -- proper colour or something
 vim.opt.termguicolors = true
+
 -- tab formatting or some shit, idk makes shit look cool
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
+
 -- disable start splash screen
 vim.opt.shortmess:append("I")
+
 -- disable line wrap
 vim.o.wrap = false
+
 -- the one keymap (shift y yanks to system clipboard)
 vim.keymap.set({ "v", "x" }, "Y", '"+y')
--- lua/plugins/Format.lua
+++ lua/plugins/Format.lua
@@ -1,30 +1,30 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" }, -- format before save
-  cmd = { "ConformInfo" },
-  config = function()
-    local conform = require("conform")
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" }, -- format before save
+	cmd = { "ConformInfo" },
+	config = function()
+		local conform = require("conform")
 
-    conform.setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "black" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        go = { "gofmt" },
-        rust = { "rustfmt" },
-        json = { "jq" },
-        markdown = { "prettier" },
-      },
-      format_on_save = {
-        lsp_fallback = true, -- ❌ don’t use LSP formatting
-        timeout_ms = 1000,
-      },
-    })
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "black" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				go = { "gofmt" },
+				rust = { "rustfmt" },
+				json = { "jq" },
+				markdown = { "prettier" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				timeout_ms = 1000,
+			},
+		})
 
-    -- Keymap: <leader>fm for formatting
-    vim.keymap.set({ "n", "v" }, "<leader>fm", function()
-      conform.format({ async = true, lsp_fallback = true })
-    end, { desc = "Format file or range with conform" })
-  end,
+		-- Keymap: <leader>fm for formatting
+		vim.keymap.set({ "n", "v" }, "<leader>fm", function()
+			conform.format({ async = true, lsp_fallback = true })
+		end, { desc = "Format file or range with conform" })
+	end,
 }
