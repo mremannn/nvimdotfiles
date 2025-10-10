6e96d1ddc4d5249db29a98701b805adabb84f07b
mremannn <67206374+mremannn@users.noreply.github.com>
2025-10-10 17:09:13 -0600

    I forgot what I changed lowkey

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,20 +1,20 @@
-return {
-	"EdenEast/nightfox.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {},
-	config = function()
-		vim.cmd.colorscheme("carbonfox")
-	end,
-}
-
 --return {
---	"rose-pine/neovim",
+--	"EdenEast/nightfox.nvim",
 --	lazy = false,
+--	priority = 1000,
+--	opts = {},
 --	config = function()
---		vim.cmd.colorscheme("rose-pine")
+--		vim.cmd.colorscheme("carbonfox")
 --	end,
 --}
+
+return {
+	"rose-pine/neovim",
+	lazy = false,
+	config = function()
+		vim.cmd.colorscheme("rose-pine")
+	end,
+}
 
 --return { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... }
 
